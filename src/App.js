ee89a6c80156f08685a081625fc8424934a57ffa
Nathan Rothenberger <nroth10@gmail.com>
2016-04-09 21:15:59 -0400

    Add Contact component. Finished episode 12.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import ContactsList from './ContactsList';
 
-let contacts = {
+let contacts = [{
+	id: 1,
 	name: 'Scott Mescudi',
 	phone: '555 555 5555'
-}
+}, {
+	id: 2,
+	name: 'Aubrey Graham',
+	phone: '555 111 5555'
+}, {
+	id: 3,
+	name: 'Kendrick Duckworth',
+	phone: '555 222 5555'
+}, {
+	id: 4,
+	name: 'Sean Carter',
+	phone: '555 333 5555'
+}, {
+	id: 5,
+	name: 'Calvin Brodus',
+	phone: '555 444 5555'
+}]
 
 class App extends React.Component {
 	render() {
 		
+		// console.log( JSON.stringify(contacts, , '/n') );
+		console.log( contacts[1] );
+		
 		return (
 			<div>
 				<h1>Contacts List</h1>
-				<ContactsList contacts={this.props.contacts}/>
+				<ContactsList contacts={this.props.contacts} />
 			</div>
 		)
 	}
--- src/ContactsList.js
+++ src/ContactsList.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Contact from './Contact';
 
 class ContactsList extends React.Component {
 	render() {
 		
 		return (
 			<ul>
-				<li>
-					{this.props.contacts.name} 
-					<h4>{this.props.contacts.phone}</h4>
-				</li>
+				{this.props.contacts.map( (contact) => {
+					return <Contact contact={contact} key={contact.id} />
+				})}
 			</ul>
 		)
 	}
