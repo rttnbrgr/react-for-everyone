510fc35c4cc609bc36604a628c30a188f1283604
Nathan Rothenberger <nroth10@gmail.com>
2016-04-10 01:34:06 -0400

    Add search filter input. Finished episode 13.

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -2,14 +2,31 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			search: 'Level Up'
+		};
+	}
+
+	updateSearch(event) {
+		// console.log(event.target.value);
+		this.setState({search: event.target.value});
+	}
+
 	render() {
 		
 		return (
-			<ul>
-				{this.props.contacts.map( (contact) => {
-					return <Contact contact={contact} key={contact.id} />
-				})}
-			</ul>
+			<div>
+				<input type="text" 
+					value={this.state.search} 
+					onChange={this.updateSearch.bind(this)} />
+				<ul>
+					{this.props.contacts.map( (contact) => {
+						return <Contact contact={contact} key={contact.id} />
+					})}
+				</ul>
+			</div>
 		)
 	}
 }
